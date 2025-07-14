9fd9578ef1cbe281db466a4ea7ede57d58d6ce7c
tehepix2 <daniel12landa@gmail.com>
2025-07-13 21:13:29 -0400

    added the weight array for the wurfer guy method

---
+++ hw/hw1/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        int[][] weightArray5 = { // 5 x 5 board
+            {2, 3, 4, 3, 2},
+            {3, 6, 6, 6, 3},
+            {4, 6, 8, 6, 4},
+            {3, 6, 6, 6, 3},
+            {2, 3, 4, 3, 2}
+        };
+
+        int[][] weightArray8 = { // 8 x 8 board; SUPER IMPORTANT DO NOT CHANGE THIS
+            {2, 3, 4, 4, 4, 4, 3, 2},
+            {3, 4, 6, 6, 6, 0, 4, 3},
+            {4, 6, 8, 8, 8, 8, 6, 4},
+            {4, 6, 8, 8, 8, 8, 6, 4},
+            {4, 6, 8, 8, 8, 8, 6, 4},
+            {4, 6, 8, 8, 8, 8, 6, 4},
+            {3, 4, 6, 6, 6, 6, 4, 3},
+            {2, 3, 4, 4, 4, 4, 3, 2}
+        };
+
+
+        int[][] tourMap = { // the tour displayed at the end will be shown by this array
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+
+
+
+    }
+
+    public int checkWeight() {
+        // check both top-left corners
+        
+    }
+}
+
