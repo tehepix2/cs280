4564e478e5a9b7313293b6f9caca2618833f17df
tehepix2 <daniel12landa@gmail.com>
2025-07-19 20:18:55 -0400

    finished program in c++

--- hw/hw1/main.cc
+++ hw/hw1/main.cc
@@ -1,17 +1,19 @@
 #include <iostream>
+#include <array>
+#include <vector>
+
 using namespace std;
 
-
-void printBoard(int **array) {
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            cout << array[i][j] << " ";
+void printArray(array<array<int, 8>, 8> board) {
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                cout << board[i][j] << "  ";
+            }
+            cout << "\n\n";
         }
-        cout << "\n";
     }
-}
 
-int checkWeight(int **board, int x, int y) { // done
+int checkWeight(array<array<int, 8>, 8> board, int x, int y) { // done
         int weight = 0;
         // check both top-left corners
         if (!((x - 1 < 0) || (y - 2 < 0))) {
@@ -68,35 +70,12 @@ int checkWeight(int **board, int x, int y) { // done
         return weight;
     }
 
-int** updateBoard(int **board) {
-        
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (board[i][j] != -1) {
-                board[i][j] = checkWeight(board, i, j);
-            }
-        }
-    }
-    return board;
-}
-
-int** makeBoard(int board[8][8]) {
-    int** arr = new int*[8];
-    for (int i = 0; i < 8; ++i) {
-        arr[i] = new int[8];
-        for (int j = 0; j < 8; ++j) {
-            arr[i][j] = board[i][j];
-        }
-    }
-    return arr;
-}
-
-int* findLowestWeight(int** board, int x, int y) {
+array<int, 2> findLowestWeight(array<array<int, 8>, 8> board, int x, int y) {
 
         // if this tracker is 0 after checking, that means no other moves are available
         int smallestTracker = 0;
         bool foundLowest = false;
-        int* smallestCoordinates = new int[2];
+        array<int, 2> smallestCoordinates;
         // check both top-left corners
         if (!((x - 1 < 0) || (y - 2 < 0))) {
             if (smallestTracker == 0) {
@@ -272,15 +251,23 @@ int* findLowestWeight(int** board, int x, int y) {
             return smallestCoordinates;
         }
         else {
-            int* smallest = new int[2];
-            smallest[0] = x;
-            smallest[1] = y;
-            return smallest;
+            return array<int,2> {x, y};
         }
         
     }
+array<array <int, 8>, 8> updateBoard(array<array <int, 8>, 8> board) {
+        array<array <int, 8>, 8> newBoard = board;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] != -1) {
+                    newBoard[i][j] = checkWeight(board, i, j);
+                }
+            }
+        }
+        return newBoard;
+    }
 
-int** findTour(int** board, int x, int y, int tourStep, int** tourBoard) {
+    array<array <int, 8>, 8> findTour(array<array <int, 8>, 8> board, int x, int y, int tourStep, array<array <int, 8>, 8> tourBoard) {
         board[x][y] = -1;
         board = updateBoard(board);
         tourBoard[x][y] = tourStep;
@@ -293,29 +280,34 @@ int** findTour(int** board, int x, int y, int tourStep, int** tourBoard) {
         }
     }
 int main() {
-    int board[8][8] = { // main board, DO NOT CHANGE THIS
+
+    array<array<int, 8>, 8> gameBoard {{  
         {2, 3, 4, 4, 4, 4, 3, 2},
-        {3, 4, 6, 0, 6, 6, 4, 3},
+        {3, 4, 6, 6, 6, 6, 4, 3},
         {4, 6, 8, 8, 8, 8, 6, 4},
         {4, 6, 8, 8, 8, 8, 6, 4},
         {4, 6, 8, 8, 8, 8, 6, 4},
         {4, 6, 8, 8, 8, 8, 6, 4},
         {3, 4, 6, 6, 6, 6, 4, 3},
         {2, 3, 4, 4, 4, 4, 3, 2}
-    };
+    }};
 
-    int** tourBoard = new int*[8];
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            tourBoard[i][j] = 0;
-        }
-    }
-    int** gameBoard = makeBoard(board);
-    // -----------------------
+    array<array<int, 8>, 8> moveBoard {{  
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0}
+    }};
+    // ---------------------
     
-    cout << "Knight's tour starting from the top-left corner of the board:\n" ;
-    cout << "Hello World!\n"; 
-    printBoard(findTour(gameBoard, 0, 0, 1, tourBoard));
+    cout << "Knight's tour starting from the top-left corner of the board: " << "\n";
+    
+    printArray(findTour(gameBoard, 0, 0, 1, moveBoard));
     return 0;
-} 
+}
+
 
